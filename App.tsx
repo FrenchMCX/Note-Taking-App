dd8f527e2d66ba2641efb0252d1f167fd7232d68
FrenchMCX <81813288+FrenchMCX@users.noreply.github.com>
2022-09-10 08:56:50 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome To My Note-Taking Portfolio App.</h1>
+      <p> This project was built using CSS, HTML, React and TypeScript </p>
+      <p> Click a button to begin! </p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-lmwcns
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-lmwcns)
